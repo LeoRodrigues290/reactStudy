b0e9f2cb50a91bf8f298e1c51446b93c977112dd
Léo <leo.rodriguessc@gmail.com>
2023-03-24 22:07:22 -0300

    Fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,

f1a4fa9769ee1b345ac308ed56dfc5cda329b6da
Léo <leo.rodriguessc@gmail.com>
2023-03-24 23:55:41 -0300

    Update

--- src/components/authentication/LoginForm.js
+++ src/components/authentication/LoginForm.js
@@ -1,114 +1,131 @@
-import Divider from 'components/common/Divider';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import SocialAuthButtons from './SocialAuthButtons';
+import React, {useState} from 'react';
+import {Button, Col, Form, Row} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {initializeApp} from 'firebase/app';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 
-const LoginForm = ({ hasLabel, layout }) => {
-  // State
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    remember: false
-  });
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
-  // Handler
-  const handleSubmit = e => {
-    e.preventDefault();
-    toast.success(`Logged in as ${formData.email}`, {
-      theme: 'colored'
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const LoginForm = ({hasLabel, layout}) => {
+    // State
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        remember: false
     });
-  };
 
-  const handleFieldChange = e => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
+    // Handler
+    const handleLogin = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, formData.email, formData.password);
+            toast.success(`Logged in as ${formData.email}`, {
+                theme: 'colored'
+            });
+        } catch (error) {
+            toast.error(error.message, {
+                theme: 'colored'
+            });
+        }
+    };
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3">
-        {hasLabel && <Form.Label>Email address</Form.Label>}
-        <Form.Control
-          placeholder={!hasLabel ? 'Email address' : ''}
-          value={formData.email}
-          name="email"
-          onChange={handleFieldChange}
-          type="email"
-        />
-      </Form.Group>
+    const handleFieldChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
 
-      <Form.Group className="mb-3">
-        {hasLabel && <Form.Label>Password</Form.Label>}
-        <Form.Control
-          placeholder={!hasLabel ? 'Password' : ''}
-          value={formData.password}
-          name="password"
-          onChange={handleFieldChange}
-          type="password"
-        />
-      </Form.Group>
+    const handleSubmit = e => {
+        e.preventDefault();
+        handleLogin();
+    };
 
-      <Row className="justify-content-between align-items-center">
-        <Col xs="auto">
-          <Form.Check type="checkbox" id="rememberMe" className="mb-0">
-            <Form.Check.Input
-              type="checkbox"
-              name="remember"
-              checked={formData.remember}
-              onChange={e =>
-                setFormData({
-                  ...formData,
-                  remember: e.target.checked
-                })
-              }
-            />
-            <Form.Check.Label className="mb-0 text-700">
-              Remember me
-            </Form.Check.Label>
-          </Form.Check>
-        </Col>
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+                {hasLabel && <Form.Label>E-mail</Form.Label>}
+                <Form.Control
+                    placeholder={!hasLabel ? 'E-mail' : ''}
+                    value={formData.email}
+                    name="email"
+                    onChange={handleFieldChange}
+                    type="email"
+                />
+            </Form.Group>
 
-        <Col xs="auto">
-          <Link
-            className="fs--1 mb-0"
-            to={`/authentication/${layout}/forgot-password`}
-          >
-            Forgot Password?
-          </Link>
-        </Col>
-      </Row>
+            <Form.Group className="mb-3">
+                {hasLabel && <Form.Label>Senha</Form.Label>}
+                <Form.Control
+                    placeholder={!hasLabel ? 'Senha' : ''}
+                    value={formData.password}
+                    name="password"
+                    onChange={handleFieldChange}
+                    type="password"
+                />
+            </Form.Group>
 
-      <Form.Group>
-        <Button
-          type="submit"
-          color="primary"
-          className="mt-3 w-100"
-          disabled={!formData.email || !formData.password}
-        >
-          Log in
-        </Button>
-      </Form.Group>
+            <Row className="justify-content-between align-items-center">
+                <Col xs="auto">
+                    <Form.Check type="checkbox" id="rememberMe" className="mb-0">
+                        <Form.Check.Input
+                            type="checkbox"
+                            name="remember"
+                            checked={formData.remember}
+                            onChange={e =>
+                                setFormData({
+                                    ...formData,
+                                    remember: e.target.checked
+                                })
+                            }
+                        />
+                        <Form.Check.Label className="mb-0 text-700">
+                            Lembrar de mim
+                        </Form.Check.Label>
+                    </Form.Check>
+                </Col>
 
-      <Divider className="mt-4">or log in with</Divider>
+                <Col xs="auto">
+                    <Link
+                        className="fs--1 mb-0"
+                        to={`/authentication/${layout}/forgot-password`}>
+                        Esqueceu a senha?
+                    </Link>
+                </Col>
+            </Row>
 
-      <SocialAuthButtons />
-    </Form>
-  );
+            <Form.Group>
+                <Button
+                    type="submit"
+                    color="primary"
+                    className="mt-3 w-100"
+                    disabled={!formData.email || !formData.password}>
+                    Entrar
+                </Button>
+            </Form.Group>
+        </Form>
+    );
 };
 
 LoginForm.propTypes = {
-  layout: PropTypes.string,
-  hasLabel: PropTypes.bool
+    layout: PropTypes.string,
+    hasLabel: PropTypes.bool
 };
 
 LoginForm.defaultProps = {
-  layout: 'simple',
-  hasLabel: false
+    layout: 'simple',
+    hasLabel: false
 };
 
 export default LoginForm;
